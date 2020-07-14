69e55726606744c2d18cfc6d4792001386be03c3
TieDyeGeek <canderson3.14@me.com>
2020-07-13 22:58:24 -0500

    Create PdfBuilder

---
+++ src/TieDyeGeek.WkHtmlToPdf/PdfBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+
+namespace TieDyeGeek.WkHtmlToPdf
+{
+	public class PdfBuilder
+	{
+		private readonly PdfConvertEnvironment _environment;
+		private readonly PdfDocument _document;
+		private readonly PdfOutput _output;
+
+		public PdfBuilder()
+		{
+			_environment = new PdfConvertEnvironment
+			{
+				TempFolderPath = Path.GetTempPath()
+			};
+			_document = new PdfDocument
+			{
+				PaperType = PaperType.Letter
+			};
+			_output = new PdfOutput();
+		}
+
+		public PdfBuilder UsingTempFolder(string tempFolderPath)
+		{
+			_environment.TempFolderPath = tempFolderPath;
+			return this;
+		}
+
+		public PdfBuilder WithPdfGenerationTimeout(int milliseconds)
+		{
+			_environment.Timeout = milliseconds;
+			return this;
+		}
+
+		public PdfBuilder WithWkHtmlToPdfPath(string path)
+		{
+			_environment.WkHtmlToPdfPath = path;
+			return this;
+		}
+
+
+
+
+
+
+
+		public void GeneratePdf()
+		{
+			PdfConvert.ConvertHtmlToPdf(_document, _environment, _output);
+		}
+	}
+}
